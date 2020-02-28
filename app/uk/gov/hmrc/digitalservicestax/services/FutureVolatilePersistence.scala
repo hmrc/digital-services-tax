93890383e679365c29d32bb009117d9d75477d9c
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2020-02-28 13:45:35 +0000

    add in FutureVolatilePersistence

---
+++ app/uk/gov/hmrc/digitalservicestax/services/FutureVolatilePersistence.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.digitalservicestax
+package services
+
+import data._
+import scala.concurrent._
+import cats.instances.future._
+
+case class FutureVolatilePersistence()(implicit ec: ExecutionContext) extends Persistence[Future] {
+
+  private def f[A](in: A): Future[A] = Future.successful(in)
+
+  val registrations = new Registrations {
+    private def V = VolatilePersistence.registrations
+
+    def get(user: String) = f(V.get(user))
+
+    def update(user: String, reg: Registration) =
+      f(V.update(user, reg))
+
+    def confirm(user: String, newRegNo: DSTRegNumber) =
+      f(V.confirm(user, newRegNo))
+  }
+
+  val returns = new Returns {
+    private def V = VolatilePersistence.returns
+    def get(reg: Registration) =
+      f(V.get(reg))
+
+    def update(reg: Registration, all: Map[Period,Return]) =
+      f(V.update(reg, all))
+
+    def update(reg: Registration, period: Period, ret: Return) =
+      f(V.update(reg, period, ret))
+    
+  }
+
+}
