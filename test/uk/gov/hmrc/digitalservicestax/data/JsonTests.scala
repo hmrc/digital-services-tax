f69cb3438ea0308587ad9949fa47c04a17cc868d
Flavian Alexandru <alexandruflavian23@gmail.com>
2020-03-12 16:55:54 -0400

    Add JSON testing

---
+++ test/uk/gov/hmrc/digitalservicestax/data/JsonTests.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.digitalservicestax.data
+
+import org.scalacheck.Arbitrary
+import org.scalatest.{Assertion, FlatSpec, Matchers, OptionValues}
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import play.api.libs.json.{Format, Json}
+import uk.gov.hmrc.digitalservicestax.TestInstances._
+import BackendAndFrontendJson._
+import enumeratum.scalacheck._
+
+class JsonTests extends FlatSpec with Matchers with ScalaCheckDrivenPropertyChecks with OptionValues {
+
+  def testJsonRoundtrip[T : Arbitrary : Format]: Assertion = {
+    forAll { sample: T =>
+      val js = Json.toJson(sample)
+
+      val parsed = js.validate[T]
+      parsed.isSuccess shouldEqual true
+      parsed.asOpt.value shouldEqual sample
+    }
+  }
+
+  it should "serialize and de-serialise a percent instance" in {
+    testJsonRoundtrip[Percent]
+  }
+
+  it should "serialize and de-serialise an Activity instance" in {
+    testJsonRoundtrip[Activity]
+  }
+
+  it should "serialize and de-serialise a Map[GroupCompany, Money]" in {
+    testJsonRoundtrip[Map[GroupCompany, Money]]
+  }
+
+  it should "serialize and de-serialise a Map[Activity, Percent]" in {
+    testJsonRoundtrip[Map[Activity, Percent]]
+  }
+}
