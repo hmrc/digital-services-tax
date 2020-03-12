0b10d8c22e3525ade0e555874720fdb12193479f
Flavian Alexandru <alexandruflavian23@gmail.com>
2020-03-12 08:28:31 -0400

    Adding foreign address tests

--- test/uk/gov/hmrc/digitalservicestax/backend_data/RosmJsonReaderSpec.scala
+++ test/uk/gov/hmrc/digitalservicestax/backend_data/RosmJsonReaderSpec.scala
@@ -58,6 +58,59 @@ class RosmJsonReaderSpec extends FlatSpec with Matchers {
     }
   }
 
+  it should "parse a ForeignAddress from a JSON object if the country code in the source JSON is not GB" in {
+    val json: JsValue = Json.parse(
+    """|{
+       |  "safeId": "XE0001234567890",
+       |  "sapNumber": "1234567890",
+       |  "agentReferenceNumber": "AARN1234567",
+       |  "isEditable": true,
+       |  "isAnAgent": false,
+       |  "isAnASAgent":false,
+       |  "isAnIndividual": true,
+       |  "organisation": {
+       |    "organisationName": "Trotters Trading (Stepney) Ltd.",
+       |    "isAGroup": true,
+       |    "organisationType": "Not Specified"
+       |  },
+       |  "address": {
+       |    "addressLine1": "Sky, 33R",
+       |    "addressLine2": "605 West 42nd Street",
+       |    "addressLine3": "New York",
+       |    "addressLine4": "",
+       |    "postalCode": "10036",
+       |    "countryCode": "US"
+       |  },
+       |  "contactDetails": {
+       |    "primaryPhoneNumber": "01332752856",
+       |    "secondaryPhoneNumber": "07782565326",
+       |    "faxNumber": "01332754256",
+       |    "emailAddress": "stephen@manncorpone.co.uk"
+       |  }
+       |}
+       |""".stripMargin)
+
+    RosmJsonReader.reads(json) shouldEqual JsSuccess(
+      CompanyRegWrapper(
+        Company(
+          NonEmptyString("Trotters Trading (Stepney) Ltd."),
+          ForeignAddress(
+            line1 = NonEmptyString("Sky, 33R"),
+            line2 = "605 West 42nd Street",
+            line3 = "New York",
+            line4 = "",
+            line5 = "",
+            postalCode = "10036",
+            CountryCode("US")
+          )
+        ),
+        None,
+        Some(SafeId("XE0001234567890")),
+        false
+      )
+    )
+  }
+
   val json: JsValue = Json.parse(
     """|{
        |  "safeId": "XE0001234567890",
