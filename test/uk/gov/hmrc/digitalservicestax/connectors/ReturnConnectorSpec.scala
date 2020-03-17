29c3df310f4edb29fb14234d2c6e0f31e64b6d9e
Flavian Alexandru <alexandruflavian23@gmail.com>
2020-03-17 11:33:49 +0000

    Enriching

--- test/uk/gov/hmrc/digitalservicestax/connectors/ReturnConnectorSpec.scala
+++ test/uk/gov/hmrc/digitalservicestax/connectors/ReturnConnectorSpec.scala
@@ -16,13 +16,48 @@
 
 package uk.gov.hmrc.digitalservicestax.connectors
 
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, get, stubFor, urlPathEqualTo}
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import uk.gov.hmrc.digitalservicestax.data.DSTRegNumber
+import uk.gov.hmrc.digitalservicestax.util.TestInstances._
 import uk.gov.hmrc.digitalservicestax.util.WiremockSpec
+import uk.gov.hmrc.http.HeaderCarrier
 
 class ReturnConnectorSpec extends WiremockSpec with ScalaCheckDrivenPropertyChecks {
 
   object ReturnTestConnector extends ReturnConnector(httpClient, environment.mode, servicesConfig, appConfig) {
     override val desURL: String = mockServerUrl
+  }
+
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  "should retrieve the latest DST period for a DSTRegNumber" in {
+    val dstRegNumber = arbitrary[DSTRegNumber].sample.value
+
+    stubFor(
+      get(urlPathEqualTo(s"""/enterprise/obligation-data/zdst/$dstRegNumber/DST"""))
+        .willReturn(aResponse()
+        .withStatus(200)))
+
+    val response = ReturnTestConnector.getNextPendingPeriod(dstRegNumber)
+    whenReady(response.failed) { res =>
+      res
+    }
+  }
+
+  "should retrieve the a list of DST periods for a DSTRegNumber" in {
+    val dstRegNumber = arbitrary[DSTRegNumber].sample.value
+
+    stubFor(
+      get(urlPathEqualTo(s"""/enterprise/obligation-data/zdst/$dstRegNumber/DST"""))
+        .willReturn(aResponse()
+        .withStatus(200)))
+
+    val response = ReturnTestConnector.getPeriods(dstRegNumber)
+    whenReady(response.failed) { res =>
+      res
+    }
   }
 
 }
