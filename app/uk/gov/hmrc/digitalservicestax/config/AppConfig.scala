a1faf82bf63103b6ac24f38e4514d22e29b1b4ec
Matt Roberts <mattrobertsky@users.noreply.github.com>
2020-03-18 11:51:50 +0000

    more precise config name

--- app/uk/gov/hmrc/digitalservicestax/config/AppConfig.scala
+++ app/uk/gov/hmrc/digitalservicestax/config/AppConfig.scala
@@ -30,6 +30,6 @@ class AppConfig @Inject()(config: Configuration, servicesConfig: ServicesConfig)
 
   val logRegResponse: Boolean = config.getOptional[Boolean]("log.register-response").getOrElse(false)
 
-  val obligationStartDate: String = config.getOptional[String]("obligation-data.start").getOrElse("2020-04-01")
+  val obligationStartDate: String = config.getOptional[String]("obligation-data.fromDate").getOrElse("2020-04-01")
 
 }
