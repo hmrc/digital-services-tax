4700151d44eccefff7ddb43a3df630adc756f012
gavinharris43 <45851646+gavinharris43@users.noreply.github.com>
2020-07-29 15:31:40 +0100

    added base spec

--- test/uk/gov/hmrc/digitalservicestax/controllers/ReturnsControllerSpec.scala
+++ test/uk/gov/hmrc/digitalservicestax/controllers/ReturnsControllerSpec.scala
@@ -21,58 +21,16 @@ import java.time.LocalDate
 
 import org.mockito.ArgumentMatchers._
 import org.mockito.Mockito._
-import org.scalatestplus.mockito._
-import org.scalatestplus.play._
-import play.api.Configuration
 import play.api.mvc._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import uk.gov.hmrc.auth.core.AuthConnector
-import uk.gov.hmrc.auth.core.retrieve.{Credentials, EmptyRetrieval}
-import uk.gov.hmrc.digitalservicestax.actions.{LoggedInAction, LoggedInRequest, Registered, RegisteredRequest}
-import uk.gov.hmrc.digitalservicestax.config.AppConfig
-import uk.gov.hmrc.digitalservicestax.connectors
-import uk.gov.hmrc.digitalservicestax.data.Period.Key
-import uk.gov.hmrc.digitalservicestax.data.{CompanyRegWrapper, ContactDetails, DSTRegNumber, Period, Registration}
-import uk.gov.hmrc.digitalservicestax.services.MongoPersistence
-import uk.gov.hmrc.http.HeaderCarrier
-import uk.gov.hmrc.play.audit.http.connector.AuditConnector
-import uk.gov.hmrc.play.bootstrap.config.RunMode
+import uk.gov.hmrc.digitalservicestax.Helpers.ControllerBaseSpec
+import uk.gov.hmrc.digitalservicestax.data.Period
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 
-class ReturnsControllerSpec extends PlaySpec with MockitoSugar with Results {
-  private val mockAuthConnector = mock[AuthConnector]
-  private val mockRunModeConfiguration = mock[Configuration]
-  private val mockRunMode = mock[RunMode]
-  private val mockAppConfig = mock[AppConfig]
-  private val mockCc = mock[ControllerComponents]
-  private val mockPersistence = mock[MongoPersistence]
-  private val mockConnector = mock[connectors.ReturnConnector]
-  private val mockAuditing = mock[AuditConnector]
-  private val mockCompanyReg = mock[CompanyRegWrapper]
-  private val mockContact = mock[ContactDetails]
-  private val mockMcc = mock[MessagesControllerComponents]
-
-
-  val regObj: Registration = Registration(mockCompanyReg, None, None, mockContact, LocalDate.now().minusWeeks(1),LocalDate.now().plusMonths(3), Some(DSTRegNumber("ASDST1010101010")))
-
-  def loginReq[A]: LoggedInRequest[A] = new LoggedInRequest[A](null, null, null, FakeRequest().withBody(AnyContent().asInstanceOf[A]))
-
-  val loginReturn: LoggedInAction = new LoggedInAction(mockMcc, mockAuthConnector) {
-    override def refine[A](request: Request[A]): Future[Either[Result, LoggedInRequest[A]]] =
-      Future.successful(Right(loginReq[A]))
-
-    override def parser: BodyParser[AnyContent] = stubBodyParser()
-  }
-
-  val mockRegistered: Registered = new Registered(mockPersistence) {
-    override def refine[A](request: LoggedInRequest[A]): Future[Either[Result, RegisteredRequest[A]]] =
-      Future.successful(Right(RegisteredRequest[A](regObj, loginReq[A])))
-  }
-
-  implicit lazy val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+class ReturnsControllerSpec extends ControllerBaseSpec {
 
   object TestReturnsController extends ReturnsController(
     authConnector = mockAuthConnector,
@@ -89,21 +47,91 @@ class ReturnsControllerSpec extends PlaySpec with MockitoSugar with Results {
 
 
   "Amendable Returns" must {
-    "return none" in {
+    val startValid1 = LocalDate.now().minusYears(2).minusWeeks(1)
+    val endValid1 = LocalDate.now().minusYears(1).minusWeeks(1)
+    val dueValid1 = LocalDate.now().minusWeeks(1)
+    val submittedValid1 = LocalDate.now().minusWeeks(50)
+    val startFailed = LocalDate.now().minusYears(4).minusWeeks(1)
+    val endFailed = startFailed.plusYears(1)
+    val dueFailed = endFailed.plusYears(1)
+    val submittedFailed = LocalDate.now().minusYears(1)
+
+
+    "return 200 with 1 period within time frame" in {
+
+
       val periods: Future[List[(Period, Option[LocalDate])]] =
         Future.successful(List(Period(
-          LocalDate.now().minusYears(2).minusWeeks(1),
-          LocalDate.now().minusYears(1).minusWeeks(1),
-          LocalDate.now().minusWeeks(1),
+          startValid1,
+          endValid1,
+          dueValid1,
           Period.Key("001")) ->
-          Some(LocalDate.now().minusWeeks(1))))
+          Some(submittedValid1)))
 
       when(mockConnector.getPeriods(any())(any(), any())) thenReturn periods
 
       val result: Future[Result] = TestReturnsController.lookupAmendableReturns().apply(FakeRequest())
 
-      val expected = status(result)
-      expected mustBe 200
+
+      val resultStatus = status(result)
+
+
+      resultStatus mustBe 200
+      contentAsString(result) mustBe s"""[{"start":"$startValid1","end":"$endValid1","returnDue":"$dueValid1","key":"001"}]"""
+
+    }
+
+    "return 200 with no periods within time frame" in {
+
+
+      val periods: Future[List[(Period, Option[LocalDate])]] =
+        Future.successful(List(Period(
+          startFailed,
+          endFailed,
+          dueFailed,
+          Period.Key("001")) ->
+          Some(submittedFailed)))
+
+      when(mockConnector.getPeriods(any())(any(), any())) thenReturn periods
+
+      val result: Future[Result] = TestReturnsController.lookupAmendableReturns().apply(FakeRequest())
+
+
+      val resultStatus = status(result)
+
+
+      resultStatus mustBe 200
+      contentAsString(result) mustBe s"""[]"""
+
+    }
+
+    "return 200 with 2 periods within time frame" in {
+
+      val startValid2 = LocalDate.now().minusYears(3).minusWeeks(1)
+      val endValid2 = startValid2.plusYears(1)
+      val dueValid2 = endValid2.plusYears(1)
+      val submittedValid2 = LocalDate.now().minusWeeks(1)
+
+      val periodList: List[(Period, Option[LocalDate])] = List(
+        Period(startValid1, endValid1, dueValid1, Period.Key("001")) -> Some(submittedValid1),
+        Period(startFailed, endFailed, dueFailed, Period.Key("002")) -> Some(submittedFailed),
+        Period(startValid2, endValid2, dueValid2, Period.Key("003")) -> Some(submittedValid2))
+
+
+      val periods: Future[List[(Period, Option[LocalDate])]] =
+        Future.successful(periodList)
+
+      when(mockConnector.getPeriods(any())(any(), any())) thenReturn periods
+
+      val result: Future[Result] = TestReturnsController.lookupAmendableReturns().apply(FakeRequest())
+
+
+      val resultStatus = status(result)
+
+
+      resultStatus mustBe 200
+      contentAsString(result) mustBe s"""[{"start":"$startValid1","end":"$endValid1","returnDue":"$dueValid1","key":"001"},{"start":"$startValid2","end":"$endValid2","returnDue":"$dueValid2","key":"003"}]"""
+
     }
 
   }
