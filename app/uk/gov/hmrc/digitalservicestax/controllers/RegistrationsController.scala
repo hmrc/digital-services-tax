79ba0bd467a21814773ebc9cd62d482cd9e6d5ef
Matt Roberts <mattrobertsky@users.noreply.github.com>
2020-10-08 10:27:54 +0100

    move debugging to call site

--- app/uk/gov/hmrc/digitalservicestax/controllers/RegistrationsController.scala
+++ app/uk/gov/hmrc/digitalservicestax/controllers/RegistrationsController.scala
@@ -79,6 +79,7 @@ class RegistrationsController @Inject()(
             request.providerId
           )
           _  <- persistence.pendingCallbacks(r.formBundleNumber) = request.internalId
+          _  =  Logger.info(s"TE put contains safeId: $safeId and formBundleNumber: ${r.formBundleNumber}")
           _  <- taxEnrolmentConnector.subscribe(safeId, r.formBundleNumber)
         } yield r
       ).auditError  (_ => AuditingHelper.buildRegistrationAudit(data, request.providerId, None, "ERROR"))
@@ -132,6 +133,7 @@ class RegistrationsController @Inject()(
               // attempt to subscribe again
               r.companyReg.safeId.fold(rosmConnector.getSafeId(r))(_.some.pure[Future]).map {
                 _.map { safeId =>
+                  Logger.info(s"TE put contains safeId: $safeId and formBundleNumber: $formBundle")
                   taxEnrolmentConnector.subscribe(
                     safeId,
                     formBundle
