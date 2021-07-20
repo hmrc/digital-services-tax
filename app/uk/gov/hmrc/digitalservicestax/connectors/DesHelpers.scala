dbd34378d931f57b51a623796042f484570b3ce4
Matt Roberts <mattrobertsky@users.noreply.github.com>
2021-07-20 11:26:54 +0100

    move authorization header out of headercarrier and send explicitly

--- app/uk/gov/hmrc/digitalservicestax/connectors/DesHelpers.scala
+++ app/uk/gov/hmrc/digitalservicestax/connectors/DesHelpers.scala
@@ -16,8 +16,9 @@
 
 package uk.gov.hmrc.digitalservicestax.connectors
 
+import play.api.http.{ContentTypes, HeaderNames}
 import play.api.libs.json.Writes
-import uk.gov.hmrc.http.{Authorization, HeaderCarrier, HttpReads}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpReads}
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import uk.gov.hmrc.play.bootstrap.http.HttpClient
 
@@ -27,20 +28,25 @@ trait DesHelpers {
 
   def http: HttpClient
   def servicesConfig: ServicesConfig
+  private def headers = Seq(
+    HeaderNames.CONTENT_TYPE -> ContentTypes.JSON,
+    "Environment" -> servicesConfig.getConfString("des.environment", ""),
+    "Authorization" -> s"Bearer ${servicesConfig.getConfString("des.token", "")}"
+  )
+
   def desGet[O](url: String)(implicit rds: HttpReads[O], hc: HeaderCarrier, ec: ExecutionContext): Future[O] =
-    http.GET[O](url)(rds, addHeaders, ec)
+    http.GET[O](url, Seq.empty, headers)(rds, addHeaders, ec)
 
   def desPost[I, O](url: String, body: I)(implicit wts: Writes[I], rds: HttpReads[O], hc: HeaderCarrier, ec: ExecutionContext): Future[O] =
-    http.POST[I, O](url, body)(wts, rds, addHeaders, ec)
+    http.POST[I, O](url, body, headers)(wts, rds, addHeaders, ec)
 
   def desPut[I, O](url: String, body: I)(implicit wts: Writes[I], rds: HttpReads[O], hc: HeaderCarrier, ec: ExecutionContext): Future[O] =
-    http.PUT[I, O](url, body)(wts, rds, addHeaders, ec)
+    http.PUT[I, O](url, body, headers)(wts, rds, addHeaders, ec)
 
   def addHeaders(implicit hc: HeaderCarrier): HeaderCarrier = {
-    hc.withExtraHeaders(
-      "Environment" -> servicesConfig.getConfString("des.environment", "")
-    ).copy(authorization = Some(Authorization(s"Bearer ${servicesConfig.getConfString("des.token", "")}")))
+    hc.copy(authorization = None)
   }
+
 
 }
 
