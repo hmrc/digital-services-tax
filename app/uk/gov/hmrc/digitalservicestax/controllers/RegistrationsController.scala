bc5e291fbae54f55257814a85a4db2bf493a6620
Matt Roberts <mattrobertsky@users.noreply.github.com>
2021-11-02 13:05:13 +0000

    remove unused/legacy registration fix

--- app/uk/gov/hmrc/digitalservicestax/controllers/RegistrationsController.scala
+++ app/uk/gov/hmrc/digitalservicestax/controllers/RegistrationsController.scala
@@ -97,63 +97,6 @@ class RegistrationsController @Inject()(
     )
   }
 
-  /** The registration may not have completed yet, or the callback was unable to return.
-    * Check to see if there is a enrolment record. 
-    */
-  def attemptRegistrationFix(r: Registration)(implicit request: LoggedInRequest[AnyContent], hc: HeaderCarrier, ec: ExecutionContext): Future[Option[Registration]] =
-  {
-    logger.warn(s"Enrolments from Auth: ${request.enrolments.enrolments.map(_.key).mkString(", ")}")
-    (for {
-      formBundle            <- OptionT(persistence.pendingCallbacks.reverseLookup(request.internalId))
-      subscription          <- OptionT.liftF(taxEnrolmentConnector.getSubscription(formBundle))
-      processedRegistration <- subscription match {
-            case s@TaxEnrolmentsSubscription(_, _, "SUCCEEDED", _) =>
-              logger.info("fetched a SUCCEEDED subscription. About to process and audit.")
-              for {
-                dstNum <- OptionT.fromOption[Future](s.getDSTNumber)
-                updatedR <- OptionT.liftF(persistence.pendingCallbacks.process(formBundle, dstNum))
-                period   <- OptionT.liftF(returnConnector.getNextPendingPeriod(dstNum))
-                _        <- OptionT.liftF(emailConnector.sendConfirmationEmail(
-                  updatedR.contact,
-                  updatedR.companyReg.company.name,
-                  updatedR.ultimateParent.fold(CompanyName("unknown")){x => x.name},
-                  dstNum,
-                  period
-                ).auditSuccess(_ => AuditingHelper.buildCallbackAudit(
-                  CallbackNotification("SUCCEEDED", None),
-                  formBundle,
-                  "SUCCESS",
-                  s.getDSTNumber)))
-              } yield updatedR
-              
-            case TaxEnrolmentsSubscription(identifiers, _, state, errors) =>
-              // we have a state other than SUCCEEDED
-              logger.warn(
-                s"state: $state, " +
-                s"errors: ${errors.getOrElse("none reported")}, " +
-                s"identifiers: ${if (identifiers.isEmpty) "none" else "some"}")
-              logger.warn("attempting TE subscribe again")
-              // attempt to subscribe again
-              r.companyReg.safeId.fold(rosmConnector.getSafeId(r))(_.some.pure[Future]).map {
-                _.map { safeId =>
-                  taxEnrolmentConnector.subscribe(
-                    safeId,
-                    formBundle
-                  ).auditError(_ => AuditingHelper.buildRegistrationAudit(
-                    r, request.providerId, None, "ERROR"
-                  )).auditSuccess(_ => AuditingHelper.buildRegistrationAudit(
-                    r, request.providerId, formBundle.some, "SUCCESS"
-                  )).map(_ => ())
-                }
-              }
-              OptionT.some[Future](r)
-          }
-        } yield processedRegistration).fold {
-          logger.warn("unable to get processed registration")
-          none[Registration]
-        }(_.some)
-  }
-
   def lookupRegistration(): Action[AnyContent] = loggedIn.async { implicit request =>
     for {
       a <- persistence.registrations.get(request.internalId)
