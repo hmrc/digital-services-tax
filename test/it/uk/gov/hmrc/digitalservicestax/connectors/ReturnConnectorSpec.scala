4f4c5e39d1dd34181d160e5181591758470ff1e4
Chandra Andem <36305926+chandraAndem@users.noreply.github.com>
2023-07-03 12:50:21 +0100

    Added one test for parsing Get Obligations response

--- test/it/uk/gov/hmrc/digitalservicestax/connectors/ReturnConnectorSpec.scala
+++ test/it/uk/gov/hmrc/digitalservicestax/connectors/ReturnConnectorSpec.scala
@@ -31,6 +31,7 @@ import it.uk.gov.hmrc.digitalservicestax.util.TestInstances._
 import it.uk.gov.hmrc.digitalservicestax.util.{FakeApplicationSetup, WiremockServer}
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.digitalservicestax.data.Period.Key
 
 import java.time.LocalDate
 
@@ -45,6 +46,93 @@ class ReturnConnectorSpec extends FakeApplicationSetup with WiremockServer with 
   object ReturnTestConnector extends ReturnConnector(httpClient, environment.mode, appConfig)
 
   implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  "should retrieve the latest DST period json for a DSTRegNumber" in {
+    val dstRegNumber = arbitrary[DSTRegNumber].sample.value
+    val periods      = s"""{
+                          |   "obligations":[
+                          |      {
+                          |         "obligationDetails":[
+                          |            {
+                          |               "status":"O",
+                          |               "inboundCorrespondenceFromDate":"2023-09-27",
+                          |               "inboundCorrespondenceToDate":"2024-09-26",
+                          |               "inboundCorrespondenceDueDate":"2025-09-26",
+                          |               "periodKey":"#005"
+                          |            },
+                          |            {
+                          |               "status":"O",
+                          |               "inboundCorrespondenceFromDate":"2022-09-27",
+                          |               "inboundCorrespondenceToDate":"2023-09-26",
+                          |               "inboundCorrespondenceDueDate":"2024-09-26",
+                          |               "periodKey":"#004"
+                          |            },
+                          |            {
+                          |               "status":"O",
+                          |               "inboundCorrespondenceFromDate":"2021-09-27",
+                          |               "inboundCorrespondenceToDate":"2022-09-26",
+                          |               "inboundCorrespondenceDueDate":"2023-09-26",
+                          |               "periodKey":"#003"
+                          |            },
+                          |            {
+                          |               "status":"F",
+                          |               "inboundCorrespondenceFromDate":"2020-09-27",
+                          |               "inboundCorrespondenceToDate":"2021-09-26",
+                          |               "inboundCorrespondenceDateReceived":"2022-06-20",
+                          |               "inboundCorrespondenceDueDate":"2022-09-26",
+                          |               "periodKey":"#002"
+                          |            },
+                          |            {
+                          |               "status":"F",
+                          |               "inboundCorrespondenceFromDate":"2020-04-01",
+                          |               "inboundCorrespondenceToDate":"2020-09-26",
+                          |               "inboundCorrespondenceDateReceived":"2021-06-22",
+                          |               "inboundCorrespondenceDueDate":"2021-09-26",
+                          |               "periodKey":"#001"
+                          |            }
+                          |         ]
+                          |      }
+                          |   ]
+                          |}""".stripMargin
+
+    val expectedResponse = List(
+      (
+        Period(LocalDate.of(2023, 9, 27), LocalDate.of(2024, 9, 26), LocalDate.of(2025, 9, 26), Key("005")),
+        None
+      ),
+      (
+        Period(LocalDate.of(2022, 9, 27), LocalDate.of(2023, 9, 26), LocalDate.of(2024, 9, 26), Key("004")),
+        None
+      ),
+      (
+        Period(LocalDate.of(2021, 9, 27), LocalDate.of(2022, 9, 26), LocalDate.of(2023, 9, 26), Key("003")),
+        None
+      ),
+      (
+        Period(
+          LocalDate.of(2020, 9, 27),
+          LocalDate.of(2021, 9, 26),
+          LocalDate.of(2022, 9, 26),
+          Key("002")
+        ),
+        Some(LocalDate.of(2022, 6, 20))
+      ),
+      (
+        Period(LocalDate.of(2020, 4, 1), LocalDate.of(2020, 9, 26), LocalDate.of(2021, 9, 26), Key("001")),
+        Some(LocalDate.of(2021, 6, 22))
+      )
+    )
+
+    stubFor(
+      get(urlPathEqualTo(s"""/enterprise/obligation-data/zdst/$dstRegNumber/DST"""))
+        .willReturn(aResponse().withStatus(200).withBody(periods))
+    )
+
+    val response = ReturnTestConnector.getPeriods(dstRegNumber)
+    whenReady(response) { res =>
+      res mustEqual expectedResponse
+    }
+  }
 
   "should retrieve the latest DST period for a DSTRegNumber" in {
     val dstRegNumber = arbitrary[DSTRegNumber].sample.value
